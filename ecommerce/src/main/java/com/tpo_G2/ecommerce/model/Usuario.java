b9dd97f7ee379b6f52bb095076f5037d1e58346d
MinionUADE <saabad@uade.edu.ar>
2026-04-14 16:34:40 +0200

    Create Usuario entity with JPA annotations

---
+++ ecommerce/src/main/java/com/tpo_G2/ecommerce/model/Usuario.java
@@ -0,0 +1,27 @@
+package com.tpo_G2.ecommerce.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "usuarios")
+public class Usuario{
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String username;
+  private String nombre;
+  private String apellido;
+  private String email;
+  private String password;
+
+  @OneToMany(mappedBy = "usuario")
+  private List<Producto> productos;
+  
+}
