83f26effc03441bf494e88515774d57b97f7054d
MinionUADE <saabad@uade.edu.ar>
2026-04-14 16:39:58 +0200

    Add ItemCarrito entity for shopping cart functionality

---
+++ ecommerce/src/main/java/com/tpo_G2/ecommerce/model/ItemCarrito.java
@@ -0,0 +1,27 @@
+package com.tpo_G2.ecommerce.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "carrito")
+public class ItemCarrito{
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private int cantidad;
+  private double precioUnitario;
+
+  @ManyToOne
+  private Producto producto;
+
+  @ManyToOne
+  private Carrito carrito;
+  
+}
