0bb6492cddf1156afad615a98540d8f2000e707c
MinionUADE <saabad@uade.edu.ar>
2026-04-15 17:58:14 +0200

    Create CarritoRepository interface for Carrito model

---
+++ ecommerce/src/main/java/com/tpo_G2/ecommerce/repository/CarritoRepository.java
@@ -0,0 +1,7 @@
+package com.tpo_G2.ecommerce.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tpo_G2.ecommerce.model.Carrito;
+
+public interface CarritoRepository extends JpaRepository<Carrito, Long> {}
