7ff286f7dda07ebe11cf418101303bbe313a90f5
Guillermo Gerstner <guillermogerstner@gmail.com>
2026-04-24 20:50:25 -0300

    Add findByProductoId method to ImagenProductoRepository
    Added method to find images by product ID.

--- ecommerce/src/main/java/com/tpo_G2/ecommerce/repository/ImagenProductoRepository.java
+++ ecommerce/src/main/java/com/tpo_G2/ecommerce/repository/ImagenProductoRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.tpo_G2.ecommerce.model.ImagenProducto;
 
 public interface ImagenProductoRepository extends JpaRepository<ImagenProducto, Long> {
-
+    // Busca dentro de 'producto' la variable 'id'
+    List<ImagenProducto> findByProductoId(Long id);
 }
