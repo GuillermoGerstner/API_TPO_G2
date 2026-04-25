ecbc785ad778846bb3e459d3217505901f4a3e79
Bruno Moriones <bmoriones@uade.edu.ar>
2026-04-24 21:02:53 -0300

    Se agregan imports faltantes

--- ecommerce/src/main/java/com/tpo_G2/ecommerce/repository/ImagenProductoRepository.java
+++ ecommerce/src/main/java/com/tpo_G2/ecommerce/repository/ImagenProductoRepository.java
@@ -1,5 +1,7 @@
 package com.tpo_G2.ecommerce.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tpo_G2.ecommerce.model.ImagenProducto;
--- ecommerce/src/main/java/com/tpo_G2/ecommerce/service/ImagenProductoService.java
+++ ecommerce/src/main/java/com/tpo_G2/ecommerce/service/ImagenProductoService.java
@@ -1,5 +1,7 @@
 package com.tpo_G2.ecommerce.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
